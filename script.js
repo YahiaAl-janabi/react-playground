27fa0bf8c0102f32c7c54e4a203f5b77d09641a9
Yahia Al-janabi <yahiaaljanabi@gmail.com>
2021-02-18 14:47:16 +0000

    up

--- script.js
+++ script.js
@@ -2,12 +2,14 @@ const theRoot = document.getElementById("root");
 
 const App = () => {
   const myItem = "the next item";
+  const myOtherItem = "This is the other item";
 
   return (
     <ul>
       <li>item1111</li>
       <li>item2</li>
       <li>{myItem.toUpperCase()}</li>
+      <li>{myOtherItem.substring(12, 17)}</li>
     </ul>
   );
 };
