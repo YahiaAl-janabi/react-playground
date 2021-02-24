4901ece9ebff5d07dadc5c8543fc61a05a5ea84f
Yahia Al-janabi <yahiaaljanabi@gmail.com>
2021-02-24 22:34:07 +0000

    up

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,10 @@
 import React from "react";
+import CountButton from "./CountButton";
 
 const App = () => {
-  const myItem = "the next item";
-  const myOtherItem = "This is the other item";
-  const theTitle = "This is the title";
   return (
     <div>
-      <ul>
-        <li>item1111</li>
-        <li>item2</li>
-        <li>{myItem.toUpperCase()}</li>
-        <li>{myOtherItem.substring(12, 17)}</li>
-        <li>{myOtherItem.length}</li>
-      </ul>
-
-      <h1>{theTitle}</h1>
+      <CountButton />
     </div>
   );
 };
